679c0c643f4e9f22dfb01f42ae30be5693710b12
Declan O'Neill <213015434+declan389@users.noreply.github.com>
2025-07-25 20:52:25 +0700

    Add game loop to play 5 rounds and display final scores

--- script.js
+++ script.js
@@ -2,21 +2,16 @@ function getComputerChoice() {
     return Math.floor(Math.random() * 3) + 1;
 }
 
-let num = getComputerChoice();
-
 const wordMap = {
     1: "rock",
     2: "paper",
     3: "scissors"
 };
-let result = wordMap[num];
-console.log(result);
 
 
 function getHumanChoice() { 
   return prompt("Enter rock, paper, or scissors:");
 }
-console.log(getHumanChoice())
 
 let humanScore = 0;
 let computerScore = 0;
@@ -53,8 +48,12 @@ function playRound(humanChoice, computerChoice) {
     }    
 }
 
+for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = wordMap[getComputerChoice()];
+    playRound(humanSelection, computerSelection);
+}
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+console.log("Final scores:");
+console.log("You:", humanScore);
+console.log("Computer:", computerScore);
